1bfef84634cf957a1ad0461faa08765ec4125d6a
Mustafa <mustafa.mustafayev482@gmail.com>
2022-08-31 12:03:03 +0400

    context2

--- context/src/App.js
+++ context/src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
+import React, { useState } from 'react';
+import Icerik from './components/icerik';
+
+
+export const TemaVerisi = React.createContext();
+
+const tema ={
+  light:{
+color:"black",
+backgroundColor:"white"
+  },
+  dark:{
+color:"white",
+backgroundColor:"black"
+  }
+};
+
 
 function App() {
+const [deger,setDeger] = useState(tema.dark);
+
+function Deyistir(){
+  if(tema.dark===deger){
+     setDeger(tema.light)
+  }
+  else{
+    setDeger(tema.dark)
+  }
+  console.log(deger);
+}
   return (
     <div className="App">
-     
+    <button onClick={Deyistir}>{tema.dark===deger ? "white":"dark"}</button>
+    <TemaVerisi.Provider value={deger}>
+    <Icerik />
+    </TemaVerisi.Provider>
     </div>
   );
 }
 
 export default App;
+
