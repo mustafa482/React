81705d96a725817e82b89b167e16a75f22c3b9c3
Mustafa <mustafa.mustafayev482@gmail.com>
2022-08-31 13:44:57 +0400

    Effect

--- useState,useEffect,useRef/src/App.js
+++ useState,useEffect,useRef/src/App.js
@@ -1,6 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
-import Header from './components/Header'
+// import Header from './components/Header'
+
+
+
+ function App() {
+
+const [timer,setTimer] = useState(5);
+
+
+useEffect(()=>{
+
+  
+  if(timer!==0){
+    const interval = setInterval(()=>{
+      setTimer(timer-1)
+      },1000)
+
+
+return ()=>clearInterval(interval);
+
+  }
+   
+
+
+
+},[timer])
+
+
+  return (
+    <div className='App'>
+   <div style={{height:"250px",width:"250px",backgroundColor:"blue",borderRadius:"50%",color:"white",fontSize:"60px",margin:"10px auto",lineHeight:"250px"}}>{timer}
+   </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function App() {
 //   const [deger,setDeger] = useState(0);
@@ -17,29 +68,29 @@ import Header from './components/Header'
 // }
 
 
-function App() {
-const [basliq,setBasliq] = useState("React Öğreniyorum");
-const numara = 1902;
+// function App() {
+// const [basliq,setBasliq] = useState("React Öğreniyorum");
+// const numara = 1902;
 
-  return (
-    <div className='App'>
-      <Container title={basliq} number={numara}>
-      Men Containram
-      </Container>
-      <button style={{width:"70px",height:"50px"}} onClick={()=>{setBasliq("React props")}}>Deyistir</button>
+//   return (
+//     <div className='App'>
+//       <Container title={basliq} number={numara}>
+//       Men Containram
+//       </Container>
+//       <button style={{width:"70px",height:"50px"}} onClick={()=>{setBasliq("React props")}}>Deyistir</button>
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-function Container (props){
-  return(
-     <div style={{width:"400px",height:"400px",border:"2px solid black",margin:"0px auto"}}>
-     {props.children}
-     <Header  baslik={props.title} />
-     </div>
-  );
-}
+// function Container (props){
+//   return(
+//      <div style={{width:"400px",height:"400px",border:"2px solid black",margin:"0px auto"}}>
+//      {props.children}
+//      <Header  baslik={props.title} />
+//      </div>
+//   );
+// }
 
 
 
